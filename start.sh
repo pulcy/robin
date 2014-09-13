df3ea5d5e6671b4777fa500ba6b4b91498b41dd9
Ewout Prangsma <ewout@prangsma.net>
2014-09-13 21:00:43 +0200

    No longer override ETCD_URL if set

--- start.sh
+++ start.sh
@@ -7,7 +7,9 @@ mkdir -p /data/logs
 mkdir -p /data/tls
 
 # Prepare environment
-export ETCD_URL=$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
+if [ -z $ETCD_URL ]; then
+	export ETCD_URL=$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
+fi
 echo "ETCD_URL=$ETCD_URL"
 
 # Start supervisord
