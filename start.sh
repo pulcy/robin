2ea268d3d0da2938d1775c84683e5c8ccabbc9f3
Ewout Prangsma <ewout@prangsma.net>
2014-09-13 19:38:55 +0200

    Remove curl test

--- start.sh
+++ start.sh
@@ -6,13 +6,9 @@ mkdir -p /data/config
 mkdir -p /data/logs
 mkdir -p /data/tls
 
-apt-get update
-apt-get curl
-
 # Prepare environment
 export ETCD_URL=$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
 echo "ETCD_URL=$ETCD_URL"
-curl -L http://$ETCD_URL/v1/keys/
 
 # Start supervisord
 /usr/bin/supervisord -c /etc/supervisord.conf
