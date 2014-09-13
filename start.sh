53b3bf3b63281f3bfbd201fae792d6f1b420fb76
Ewout Prangsma <ewout@prangsma.net>
2014-09-13 19:08:23 +0200

    Changed confd args

--- start.sh
+++ start.sh
@@ -10,9 +10,9 @@ apt-get update
 apt-get curl
 
 # Prepare environment
-export ETCD_URL=http://$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
+export ETCD_URL=$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
 echo "ETCD_URL=$ETCD_URL"
-curl -L $ETCD_URL/v1/keys/
+curl -L http://$ETCD_URL/v1/keys/
 
 # Start supervisord
 /usr/bin/supervisord -c /etc/supervisord.conf
