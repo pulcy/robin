eb14dc47f5049e72d3cf14f0636674fe40f7ab28
Ewout Prangsma <ewout@prangsma.net>
2014-09-13 18:57:32 +0200

    Added curl test

--- start.sh
+++ start.sh
@@ -6,9 +6,13 @@ mkdir -p /data/config
 mkdir -p /data/logs
 mkdir -p /data/tls
 
+apt-get update
+apt-get curl
+
 # Prepare environment
 export ETCD_URL=http://$ETCD_PORT_4001_TCP_ADDR:$ETCD_PORT_4001_TCP_PORT
 echo "ETCD_URL=$ETCD_URL"
+curl -L $ETCD_URL/v1/keys/
 
 # Start supervisord
 /usr/bin/supervisord -c /etc/supervisord.conf
