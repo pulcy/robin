e7fd98ad925d256229247c076daded80944d2ba6
Ewout Prangsma <ewout@prangsma.net>
2014-09-25 14:01:48 +0200

    Changed reload approach to avoid nginx worker process crashes (signal 11)

--- reload.sh
+++ reload.sh
@@ -3,5 +3,7 @@
 # Reload nginx
 pid=$(cat /nginx.pid)
 echo "Reloading nginx ($pid)"
-kill -HUP $pid
+/usr/bin/supervisorctl -s unix:///supervisor.sock restart nginx
+#/usr/local/sbin/nginx -s reload
+#kill -HUP $pid
 
