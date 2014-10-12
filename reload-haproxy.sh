7d52080837ec29b2d527e57faee703a9607c62f8
Ewout Prangsma <ewout@prangsma.net>
2014-10-12 11:31:24 +0200

    Added mutex to restart haproxy. This avoids multiple gohst instances

--- reload-haproxy.sh
+++ reload-haproxy.sh
@@ -1,5 +1,45 @@
 #!/bin/bash
 
+create_lock_or_wait () {
+  path="$1"
+  wait_time="${2:-10}"
+  while true; do
+        if mkdir "${path}.lock.d"; then
+           break;
+        fi
+        sleep $wait_time
+  done
+}
+
+remove_lock () {
+  path="$1"
+  rmdir "${path}.lock.d"
+}
+
+# Create a mutex to prevent multiple haproxy instances
+echo "Creating lock"
+create_lock_or_wait /var/run/haproxy
+
 # Reload haproxy
 echo "Reloading haproxy"
-haproxy -D -f /data/config/haproxy.cfg -p /var/run/haproxy.pid -st $(cat /var/run/haproxy.pid)
+PIDS=$(cat /var/run/haproxy.pid)
+haproxy -D -f /data/config/haproxy.cfg -p /var/run/haproxy.pid -st $PIDS
+
+# Wait for old processes to terminate
+if [ ! -z "$PIDS" ]; then
+	counter=0
+	echo "Waiting for old instances to terminate: $PIDS"
+	while ps -p $PIDS; do
+		if [[ "$counter" -gt 20 ]]; then
+			echo "Killing processes"
+			kill -9 $PIDS
+		else
+			counter=$((counter+1))
+			echo "..."
+			sleep 1
+		fi
+	done
+fi
+
+echo "Removing lock"
+remove_lock /var/run/haproxy
